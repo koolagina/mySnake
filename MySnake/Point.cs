1f59f4d5d740759163d8e5df65ddfdf541c8673c
koolagina <koolagina@gmail.com>
2021-11-29 16:17:15 +0500

    some changes new VS

--- MySnake/Point.cs
+++ MySnake/Point.cs
@@ -1,6 +1,15 @@
 ﻿namespace MySnake
 {
     internal class Point
-    {
+
+    { public int x;
+        public int y;
+        public char sym;
+        public void Draw();
+
+        Console.SetCursorPosition(x, y)
+        Console.Write(sym);
+     
     }
+
 }
--- MySnake/Program.cs
+++ MySnake/Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace MySnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 1;
+            p1.Y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.X = 4;
+            p2.Y = 5;
+            p2.sym = '#';
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
 
